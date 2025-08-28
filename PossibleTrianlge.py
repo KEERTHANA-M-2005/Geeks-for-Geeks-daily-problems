53c4886d07187e86872a3d5a2c03640ebf82be7a
KEERTHANA-M-2005 <keerthigowli05@gmail.com>
2025-08-29 00:42:34 +0530

    Day 7

---
+++ PossibleTrianlge.py
@@ -0,0 +1,27 @@
+# Given an integer array arr[]. 
+# Find the number of triangles that can be formed with three different array elements as lengths of three sides of the triangle. A triangle with three given sides is only possible if sum of any two sides is always greater than the third side.
+
+# Examples:
+
+# Input: arr[] = [4, 6, 3, 7]
+# Output: 3
+# Explanation: There are three triangles possible [3, 4, 6], [4, 6, 7] and [3, 6, 7]. Note that [3, 4, 7] is not a possible triangle. 
+
+def NumofPossibleTriangle(arr):
+    n = len(arr)
+    arr.sort()
+    count = 0
+    for k in range(n-1, 1, -1):
+        i, j = 0, k-1
+        
+        while i < j:
+            if arr[i] + arr[j] > arr[k]:
+                count += (j-i)
+                j -= 1
+            else:
+                i += 1
+    return count
+
+arr = [10, 21, 22, 100, 101, 200, 300]
+res = NumofPossibleTriangle(arr)
+print(res)
