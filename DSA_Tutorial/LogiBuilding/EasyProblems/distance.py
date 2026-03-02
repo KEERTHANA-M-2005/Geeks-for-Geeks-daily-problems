b14e306354bb02f4643bf6afe160260ec9e0ff61
KEERTHANA-M-2005 <keerthigowli05@gmail.com>
2026-03-02 19:25:41 +0530

    Distance btw two points

---
+++ DSA_Tutorial/LogiBuilding/EasyProblems/distance.py
@@ -0,0 +1,11 @@
+# Given coordinates of 2 points on a cartesian plane, find the distance between them round the result to the nearest integer.
+
+# Example 1:
+
+# Input: 0 0 2 -2
+# Output: 3
+# Explanation: Distance between (0, 0) 
+# and (2, -2) is 3.
+import math
+def difference(x1, x2, y1, y2):
+    return round(math.sqrt((x2-x1)**2 + (y2-y1)**2))
